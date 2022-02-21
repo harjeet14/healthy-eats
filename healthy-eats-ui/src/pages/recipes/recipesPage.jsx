af05c781cf9750742b5629e233db5197e4307824
fateme <fateme3611@yahoo.com>
2022-02-21 14:45:34 -0500

    fix broken git repo

--- healthy-eats-ui/src/pages/recipes/recipesPage.jsx
+++ healthy-eats-ui/src/pages/recipes/recipesPage.jsx
@@ -1,59 +1,58 @@
-import { UnderConstruction } from "../../components/underConstruction/underConstruction";
-import { Link, Route, Routes, useNavigate } from "react-router-dom";
 import HealthyEatsApiService from '../../services/healthyEatsApiService';
 import RecipeCard from "../../components/recipes/recipeCard";
 import { RecipeInfoModal } from "../recipeInfoModal/recipeInfoModal";
-import { Grid, Container, Box, Button, TextField } from "@mui/material";
-import styles from "../recipes/recipesPage.scss"
+import { Grid, Container } from "@mui/material";
+import "../recipes/recipesPage.scss"
+import { useEffect, useState } from 'react';
+import { NewRecipe } from '../newRecipe/newRecipe';
 
-import {NewRecipe} from "../newRecipe/NewRecipe"
-import { useState, useEffect } from "react";
 
 export function RecipesPage() {
-    const [recipes, setRecipes] = useState([]);
-    const [isNewRecipeActive, setIsNewRecipeActive] = useState(false);
-    const [selectedRecipe, setSelectedRecipe] = useState(null);
+  const [recipes, setRecipes] = useState([]);
+  const [isNewRecipeActive, setIsNewRecipeActive] = useState(false);
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
 
 
-    const handleNewRecipeClick=(e)=>{
-        e.preventDefault();
-        setIsNewRecipeActive(true)
+  const handleNewRecipeClick = (e) => {
+    e.preventDefault();
+    setIsNewRecipeActive(true)
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+
+      let newRecipes = await HealthyEatsApiService.getNewRecipes(sessionStorage.sessionUserId);
+      let newRecipes2 = newRecipes.map((newRecipe) => {
+        return (
+          {
+            ...newRecipe,
+            foodImage: newRecipe.recipe_image_urls
+          }
+        )
+      })
+      console.log("new recipes", newRecipes)
+      console.log("new recipes2", newRecipes2)
+
+
+
+      setRecipes(newRecipes2)
+
     }
+    console.log("recipes", recipes)
+    fetchData();
+  }, [])
 
-    useEffect( ()=>{
-      async function fetchData(){
-
-        let newRecipes = await HealthyEatsApiService.getNewRecipes(sessionStorage.sessionUserId);
-        let newRecipes2  = newRecipes.map((newRecipe)=>{
-          return(
-            {...newRecipe,
-              foodImage: newRecipe.recipe_image_urls
-            }
-          )
-        })
-        console.log("new recipes", newRecipes)
-        console.log("new recipes2", newRecipes2)
-
-
-
-        setRecipes(newRecipes2)
-            
-      }
-      console.log("recipes", recipes)
-       fetchData();
-    }, [])
-
-    return (
-      <div>
+  return (
+    <div className="recipesPage">
       <Container>
-   
+
         <Grid container marginX={20} spacing={{ lg: 2 }} columns={{ lg: 4 }} >
           {recipes.map((recipe, index) =>
             <Grid item lg={1} key={index}>
               <RecipeCard
                 onClick={async (food) => {
 
-                  setSelectedRecipe({...recipe, image: recipe.foodImage, summary: recipe.recipe_description, instructions: recipe.recipe_instructions});
+                  setSelectedRecipe({ ...recipe, image: recipe.foodImage, summary: recipe.recipe_description, instructions: recipe.recipe_instructions });
                 }}
                 key={`recipe-${recipe.foodId}`}
                 recipe={recipe}
@@ -68,13 +67,13 @@ export function RecipesPage() {
         }} />
       }
       <button onClick={handleNewRecipeClick}>Or add your own!</button>
-        {isNewRecipeActive && <NewRecipe handleClick={()=>{setIsNewRecipeActive(false)}} />}
+      {isNewRecipeActive && <NewRecipe handleClick={() => { setIsNewRecipeActive(false) }} />}
     </div>
-     
-
-       
 
 
-  
-    );
-  };
+
+
+
+
+  );
+};
